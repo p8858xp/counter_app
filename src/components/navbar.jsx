729640fc33b481542e4c8e63d700bf0a9185e97c
Paul Park <paulpark@pauls-mbp.attlocal.net>
2019-05-24 12:15:28 -0700

    testing continuous integration

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
-        NavBar
+        NavBar Test
         <span className="badge badge-pill badge-secondary">
           {totalCounters}
         </span>
